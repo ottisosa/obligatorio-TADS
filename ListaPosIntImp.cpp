b8624c5fcf2f84282fbf3a61d32f451eaa2b896f
ottisosa <ottisosa10@gmail.com>
2026-06-09 18:30:46 -0300

    ver si hay errores en ListaPos Int

--- ListaPosIntImp.cpp
+++ ListaPosIntImp.cpp
@@ -5,41 +5,105 @@
 //Si necestita otra estructura se puede definir aqui
 
 struct _representacionListaPosInt {
-	//IMPLEMENTAR SOLUCION
+
+	unsigned int capacidad;
+	unsigned int cantElem;
+	int* datos;
+
+
 };
 
 ListaPosInt crearListaPosInt()
 {
-	//IMPLEMENTAR SOLUCION
-	return NULL;
+
+	ListaPosInt nuevo = new _representacionListaPosInt;
+
+	nuevo->cantElem = 0;
+	nuevo->capacidad = 10;  // Reservo 10 lugares de inicio
+	nuevo->datos = new int[nuevo->capacidad];
+
+	return nuevo;
 }
 
 void agregar(ListaPosInt& l, int e, unsigned int pos)
 {
-	//IMPLEMENTAR SOLUCION
+	if (l != NULL)
+	{
+		if (l->cantElem == l->capacidad)
+		{
+
+			int capacidadNuevo = l->capacidad * 2;
+			int* nuevo = new int[capacidadNuevo];
+
+			for (int i = 0; i < l->capacidad; i ++) {
+			
+				nuevo[i] = l->datos[i];
+			}
+		
+			delete[] l->datos;
+
+			l->datos = nuevo;
+			l->capacidad = capacidadNuevo;
+		}
+
+
+		if (pos >= l->cantElem)
+		{
+			pos = l->cantElem;
+		}
+
+
+		for (int i = l->cantElem; i > pos ; i--)
+		{
+			l->datos[i] = l->datos[i - 1];
+		}
+
+
+		l->datos[pos] = e;
+		l->cantElem++;
+
+
+	}
 }
 
 void borrar(ListaPosInt& l, unsigned int pos)
 {
-	//IMPLEMENTAR SOLUCION
+	if (l!= NULL)
+	{
+
+		if (pos < l->cantElem)
+		{
+			for (unsigned int i = pos; i < l->cantElem - 1; i++)
+			{
+
+				l->datos[i] = l->datos[i + 1];
+
+			}
+
+
+			l->cantElem--;
+
+		}
+	}
 }
 
 int elemento(ListaPosInt l, unsigned int pos)
 {
-	//IMPLEMENTAR SOLUCION
-	return 0;
+
+	// se asume que l no es NULL por las pecondiciones
+
+	return l->datos[pos];
 }
 
 bool esVacia(ListaPosInt l)
 {
-	//IMPLEMENTAR SOLUCION
-	return true;
+
+	return (l == NULL) || (l->cantElem == 0);
 }
 
 unsigned int cantidadElementos(ListaPosInt l)
 {
-	//IMPLEMENTAR SOLUCION
-	return 0;
+	return l->cantElem;
 }
 
 ListaPosInt clon(ListaPosInt l)
